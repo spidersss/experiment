083d443c4f91587427a3270a95a598eb64ec1c1c
spidersss <604815982@qq.com>
2019-02-19 21:38:56 +0800

    add

--- driverless_release/gps_anpp/src/print.cc
+++ driverless_release/gps_anpp/src/print.cc
@@ -4,6 +4,7 @@ ros::Subscriber gps_sub;
 void gpsCallback(const gps_anpp::gps_data::ConstPtr& gps_msg)
 {
 	if(gps_msg->lat > 30.0)	printf("%.8f,%.8f\n", gps_msg->lat + 0.000145, gps_msg->lon - 0.000356);
+	//if(gps_msg->lat > 30.0)	printf("%.8f,%.8f\n", gps_msg->lat + 0.000140, gps_msg->lon - 0.000556);
 }
 
 int main(int argc, char **argv)
